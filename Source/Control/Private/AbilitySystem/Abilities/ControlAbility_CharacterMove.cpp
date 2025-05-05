0eff559e27bf4b9a33b0856207af269bcaece035
Eric Gomes Gregory <49287457+EricGomesGregory@users.noreply.github.com>
2025-05-05 11:36:19 -0300

    Update ControlAbility_CharacterMove.cpp

--- Source/Control/Private/AbilitySystem/Abilities/ControlAbility_CharacterMove.cpp
+++ Source/Control/Private/AbilitySystem/Abilities/ControlAbility_CharacterMove.cpp
@@ -2,12 +2,12 @@
 
 
 #include "AbilitySystem/Abilities/ControlAbility_CharacterMove.h"
+#include "Player/ControlPlayerInputInterface.h"
 #include "GameFramework/Character.h"
 
 
 bool UControlAbility_CharacterMove::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, OUT FGameplayTagContainer* OptionalRelevantTags) const {
-	if (!Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags))
-	{
+	if (!Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags)) {
 		return false;
 	}
 
@@ -16,13 +16,31 @@ bool UControlAbility_CharacterMove::CanActivateAbility(const FGameplayAbilitySpe
 }
 
 void UControlAbility_CharacterMove::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) {
-	if (HasAuthorityOrPredictionKey(ActorInfo, &ActivationInfo))
-	{
-		if (!CommitAbility(Handle, ActorInfo, ActivationInfo)) {
-			return;
-		}
+	if (HasAuthorityOrPredictionKey(ActorInfo, &ActivationInfo) == false) {
+		return;
+	}
 
-		ACharacter* Character = CastChecked<ACharacter>(ActorInfo->AvatarActor.Get());
-		// @Eric TODO: Handle input
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo)) {
+		return;
+	}
+
+	ACharacter* Character = CastChecked<ACharacter>(ActorInfo->AvatarActor.Get());
+
+	// Handles player input checking
+	if (auto* PlayerInputInterface = Cast<IControlPlayerInputInterface>(Character)) {
+		FVector2D ActionValue = PlayerInputInterface->GetMoveActionValue();
+
+		if (ActionValue.IsZero()) return;
+
+		const FRotator Rotation = Character->GetControlRotation();
+		const FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f);
+
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		// Moves current controlled character/pawn in the forward direction
+		Character->AddMovementInput(ForwardDirection, ActionValue.Y);
+		// Moves current controlled character/pawn in the right direction
+		Character->AddMovementInput(RightDirection, ActionValue.X);
 	}
 }
