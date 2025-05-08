53330e565acfbb50cbb697ef32c520cadaf8370b
Eric Gomes Gregory <49287457+EricGomesGregory@users.noreply.github.com>
2025-05-08 10:20:53 -0300

    Update AbilityTask_GrantInteraction.cpp
    Fixed compiler error error C2139: 'FOverlapResult': an undefined class is not allowed as an argument to compiler intrinsic type trait '__is_trivially_destructible' by importing Engine/OverlapResult.h

--- Source/Control/Private/Interaction/Tasks/AbilityTask_GrantInteraction.cpp
+++ Source/Control/Private/Interaction/Tasks/AbilityTask_GrantInteraction.cpp
@@ -5,6 +5,7 @@
 #include "Interaction/InteractableTarget.h"
 #include "Interaction/InteractionStatics.h"
 #include "AbilitySystemComponent.h"
+#include "Engine/OverlapResult.h"
 
 
 UAbilityTask_GrantInteraction::UAbilityTask_GrantInteraction(const FObjectInitializer& ObjectInitializer)
@@ -41,38 +42,37 @@ void UAbilityTask_GrantInteraction::QueryInteractables() {
 	if (World && ActorOwner) {
 		FCollisionQueryParams Params(SCENE_QUERY_STAT(UAbilityTask_GrantInteraction), false);
 
-		// @Eric TODO: Fix error C2139: 'FOverlapResult': an undefined class is not allowed as an argument to compiler intrinsic type trait '__is_trivially_destructible'
-		//TArray<FOverlapResult> OverlapResults;
-		//World->OverlapMultiByChannel(OUT OverlapResults, ActorOwner->GetActorLocation(), FQuat::Identity, ECC_EngineTraceChannel1 /*Control_TraceChannel_Interaction*/, FCollisionShape::MakeSphere(InteractionScanRange), Params);
-		//
-		//if (OverlapResults.Num() <= 0) {
-		//	return;
-		//}
+		TArray<FOverlapResult> OverlapResults;
+		World->OverlapMultiByChannel(OUT OverlapResults, ActorOwner->GetActorLocation(), FQuat::Identity, ECC_EngineTraceChannel1 /*Control_TraceChannel_Interaction*/, FCollisionShape::MakeSphere(InteractionScanRange), Params);
+		
+		if (OverlapResults.Num() <= 0) {
+			return;
+		}
 
-		//TArray<TScriptInterface<IInteractableTarget>> InteractableTargets;
-		//UInteractionStatics::AppendInteractableTargetsFromOverlapResults(OverlapResults, OUT InteractableTargets);
+		TArray<TScriptInterface<IInteractableTarget>> InteractableTargets;
+		UInteractionStatics::AppendInteractableTargetsFromOverlapResults(OverlapResults, OUT InteractableTargets);
 
-		//FInteractionQuery InteractionQuery;
-		//InteractionQuery.RequestingAvatar = ActorOwner;
-		//InteractionQuery.RequestingController = Cast<AController>(ActorOwner->GetOwner());
+		FInteractionQuery InteractionQuery;
+		InteractionQuery.RequestingAvatar = ActorOwner;
+		InteractionQuery.RequestingController = Cast<AController>(ActorOwner->GetOwner());
 
-		//TArray<FInteractionOption> Options;
-		//for (TScriptInterface<IInteractableTarget>& InteractiveTarget : InteractableTargets) {
-		//	FInteractionOptionBuilder InteractionBuilder(InteractiveTarget, Options);
-		//	InteractiveTarget->GatherInteractionOptions(InteractionQuery, InteractionBuilder);
-		//}
+		TArray<FInteractionOption> Options;
+		for (TScriptInterface<IInteractableTarget>& InteractiveTarget : InteractableTargets) {
+			FInteractionOptionBuilder InteractionBuilder(InteractiveTarget, Options);
+			InteractiveTarget->GatherInteractionOptions(InteractionQuery, InteractionBuilder);
+		}
 
-		//// Check if any of the options need to grant the ability to the user before they can be used.
-		//for (FInteractionOption& Option : Options) {
-		//	if (Option.InteractionAbilityToGrant) {
-		//		// Grant the ability to the GAS, otherwise it won't be able to do whatever the interaction is.
-		//		FObjectKey ObjectKey(Option.InteractionAbilityToGrant);
-		//		if (!InteractionAbilityCache.Find(ObjectKey)) {
-		//			FGameplayAbilitySpec Spec(Option.InteractionAbilityToGrant, 1, INDEX_NONE, this);
-		//			FGameplayAbilitySpecHandle Handle = AbilitySystemComponent->GiveAbility(Spec);
-		//			InteractionAbilityCache.Add(ObjectKey, Handle);
-		//		}
-		//	}
-		//}
+		// Check if any of the options need to grant the ability to the user before they can be used.
+		for (FInteractionOption& Option : Options) {
+			if (Option.InteractionAbilityToGrant) {
+				// Grant the ability to the GAS, otherwise it won't be able to do whatever the interaction is.
+				FObjectKey ObjectKey(Option.InteractionAbilityToGrant);
+				if (!InteractionAbilityCache.Find(ObjectKey)) {
+					FGameplayAbilitySpec Spec(Option.InteractionAbilityToGrant, 1, INDEX_NONE, this);
+					FGameplayAbilitySpecHandle Handle = AbilitySystemComponent->GiveAbility(Spec);
+					InteractionAbilityCache.Add(ObjectKey, Handle);
+				}
+			}
+		}
 	}
 }
